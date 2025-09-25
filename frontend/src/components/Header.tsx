84f495de98325f477621da50beb053a996e8cc0a
Nikita <nikitamokhonko@gmail.com>
2025-09-25 17:15:31 +0200

    Removed unused imports

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,15 @@
-import { useNavigate } from "@tanstack/react-router";
 import Marquee from "react-fast-marquee";
 
 export default function Header() {
-  const navigate = useNavigate();
-
   return (
     <header>
       <div className="border-b-1 border-[#0B2E42] bg-[#0B2E42]">
         <div className="flex flex-row px-2 py-2 space-x-5 items-center cursor-pointer">
-          <img alt="SkinSmart Logo" className="w-16 h-16" src="skinsmart-logo.png" />
+          <img
+            alt="SkinSmart Logo"
+            className="w-16 h-16"
+            src="skinsmart-logo.png"
+          />
           <h1 className="text-4xl text-white">SkinSmart</h1>
         </div>
       </div>
