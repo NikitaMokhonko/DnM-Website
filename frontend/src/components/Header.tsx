1aa83144473a2a6a7ddfeed9341ff11f1d0ec98b
Nikita <nikitamokhonko@gmail.com>
2025-09-25 17:48:06 +0200

    Updated header for mobile view

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <header>
       <div className="border-b-1 border-[#0B2E42] bg-[#0B2E42]">
-        <div className="flex flex-row px-2 py-2 space-x-5 items-center cursor-pointer">
+        <div className="flex flex-row px-2 py-2 justify-center sm:justify-start space-x-2 sm:space-x-5 items-center">
           <img
             alt="SkinSmart Logo"
-            className="w-16 h-16"
+            className="w-16 h-16 cursor-pointer"
             src="skinsmart-logo.png"
           />
-          <h1 className="text-4xl text-white">SkinSmart</h1>
+          <h1 className="text-4xl text-white cursor-pointer">SkinSmart</h1>
         </div>
       </div>
 
