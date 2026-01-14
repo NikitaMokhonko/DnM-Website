f80b878294ff01ee57b6be062b335c3f5c56d66d
Nikita Mokhonko <nikitamokhonko@gmail.com>
2026-01-14 15:46:06 +0100

    Fixed analytics bug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
